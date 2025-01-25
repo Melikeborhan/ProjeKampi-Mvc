94c01c8e4a3e11c8aeb1f24753d2e7252aac89a9
Melike <22410051025@ogr.erbakan.edu.tr>
2025-01-25 12:21:44 +0300

    Deneme-Controller ve Layout

---
+++ ProjeKampi-Mvc/Controllers/testController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProjeKampi_Mvc.Controllers
+{
+    public class testController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Test2()
+        {
+            return View();
+        }
+    }
+}
