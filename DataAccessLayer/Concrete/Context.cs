d847fd57d6ffeb0f64ddccc08044591b71d19c0a
Melike <22410051025@ogr.erbakan.edu.tr>
2025-01-27 15:27:40 +0300

    Context sınıfı
    context sınıfı olusturma ve EntityLaye katmanını referans ekleme

---
+++ DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,30 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete
+{
+    public class Context: DbContext 
+    {
+
+        //baska katmanın sınıfını methodunu propunu kullanmak ıstıyrosan o katmanı buraya referans olarak eklemek zorundayız.
+        public DbSet<About> Abouts { get; set; }  //abouts verıtabanındaki tablo ısmı about ıse proje sınıfı ıcınde yer alan ısmıdır.
+
+        public DbSet<Category> Categories { get; set; }
+
+        public DbSet<Contact> Contacts { get; set; }
+
+        public DbSet<Content> Contents { get; set; }
+
+        public DbSet<Heading> Headings { get; set; }
+
+        public DbSet<Writer> Writers { get; set; }
+
+
+
+    }
+}
